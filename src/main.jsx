d3be6696822c9d2c169fb1ae9917b74cfc007648
Suhayb Jirde <dribblekayga@gmail.com>
2023-08-03 18:54:47 -0800

    project setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider, 
+  Route 
+} from 'react-router-dom'
+import Home from './pages/Home'
+import CountryDetails from './pages/CoutryDetails'
+import Layout from './components/Layout'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route index element={<Home />}/>
+      <Route path='/country-details' element={<CountryDetails />}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
---
+++ src/pages/CoutryDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CountryDetails = ()=> {
+    return <h1>Country Info page</h1>
+}
+
+export default CountryDetails
