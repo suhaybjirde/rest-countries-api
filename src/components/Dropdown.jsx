3f3a58a9274a05eb658a012c56fc985f7219d313
Suhayb Jirde <dribblekayga@gmail.com>
2023-08-05 22:31:44 -0800

    Filter the countries by Region

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import arrowDownImg from '../assets/images/chevron-down-outline-black.svg';
+const Dropdown = ({ handleFilterChange, region })=> {
+    const [isExpanded, setIsExpanded] = useState(false)
+    const displayButtonList = {
+        display: isExpanded ? 'block' : 'none'
+    }
 
-const Dropdown = ()=> {
+    function show() {
+        setIsExpanded(prevExpanded => !prevExpanded)
+    }
     return (
         <div className="drop-down">
-            <div>
-                <h1>true</h1>
+            <div>{
+                region ? (
+                    <>
+                        <span>{region}</span>
+                        <button 
+                            onClick={()=> {
+                                handleFilterChange({})  
+                                setIsExpanded(prevExpanded => !prevExpanded)
+                            }
+                        }
+                        >X</button>
+                    </>
+                ):
+                (
+                    <button onClick={show}>
+                        <span>Filter by Region</span>
+                        <img src={arrowDownImg} alt="drop down" width='20px'/>
+                    </button>
+                )
+            }
             </div>
+            <div style={displayButtonList}>
+                <button onClick={()=> handleFilterChange({filter: 'africa'})}>Africa</button>
+                <button onClick={()=> handleFilterChange({filter: 'america'})}>America</button>
+                <button onClick={()=> handleFilterChange({filter: 'asia'})}>Asia</button>
+                <button onClick={()=> handleFilterChange({filter: 'europe'})}>Europe</button>
+                <button onClick={()=> handleFilterChange({filter: 'oceania'})}>Oceania</button>
         </div>
+        </div>  
     )
 }
 
--- src/components/RenderCountries.jsx
+++ src/components/RenderCountries.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import Dropdown from './Dropdown'
 import searchImg from '../assets/images/search-outline-black.svg'
 import CoutryCard from './CountryCard'
 
 const RenderCountries = (LoadedCountries)=> {
     const [searchedCountry, setSearchCountry] = useState('')
-    const [filteredByRegion, setFilteredByRegion] = useState('')
-
+    const [searchParamas, setSearchParamas] = useSearchParams()
+    const filterByRegion = searchParamas.get('filter') || ''
 
     function searchCountry(event) {
         const { value } = event.target
         setSearchCountry(value)
     }
 
-   
+    function handleFilterChange(value) {
+        setSearchParamas(value)
+    }
+
     const countriesElemenst = 
         LoadedCountries
-            .filter(country => (country.region).toLowerCase().includes(filteredByRegion))
+            .filter(country => (country.region).toLowerCase().includes(filterByRegion))
             .filter(country => (country.name.common).toLowerCase().includes(searchedCountry.toLowerCase()))
             .map((country, index) => {
                 return <CoutryCard key={index} country={country}/>
@@ -29,7 +33,7 @@ const RenderCountries = (LoadedCountries)=> {
                     <img src={searchImg} alt="" width='20px'/>
                     <input type="text" name="search" placeholder="Search for a country..." onChange={searchCountry}/>
                 </div>
-                <Dropdown />
+                <Dropdown handleFilterChange={handleFilterChange} region={filterByRegion} />
             </div>
             <div className="coutries">
                 {countriesElemenst}
