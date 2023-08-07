6d79bd4e3bf62314a35f657d6c102709b31c41bd
Suhayb Jirde <dribblekayga@gmail.com>
2023-08-06 23:50:37 -0800

    style country-card

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,26 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ country })=> {
     
     return (
-        <article className="country-card">
-            <img src={country.flags.png} alt="country flag" className="country-card__img"/>
-            <div className="country-card__info">
-                <h2 className="country-card__info__title">{country.name.common}</h2>
-                <div className="country-card__info__detail">
-                    <strong>Population</strong>
-                    <span>{country.population}</span>
+        <Link to='/country-details'>
+            <article className="country-card">
+                <img src={country.flags.png} alt="country flag" className="country-card__img"/>
+                <div className="country-card__info">
+                    <h2 className="country-card__info__title">{country.name.common}</h2>
+                    <div className="country-card__info__detail">
+                        <strong>Population: </strong>
+                        <span>{country.population}</span>
+                    </div>
+                    <div className="country-card__info__detail">
+                        <strong>Region: </strong>
+                        <span>{country.region}</span>
+                    </div>
+                    <div className="country-card__info__detail">
+                        <strong>Capital: </strong>
+                        <span>{country.capital}</span>
+                    </div>
                 </div>
-                <div className="country-card__info__detail">
-                    <strong>Region</strong>
-                    <span>{country.region}</span>
-                </div>
-                <div className="country-card__info__detail">
-                    <strong>Capital</strong>
-                    <span>{country.capital}</span>
-                </div>
-            </div>
-        </article>
+            </article>
+        </Link>
     )
 }
 
