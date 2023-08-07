46b48da506ee966cffdb99588242a1316066981e
Suhayb Jirde <dribblekayga@gmail.com>
2023-08-07 10:07:31 -0800

    fix some typos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ()=> {
     return (
         <header className="header">
             <div className="container">
-                <h1 className="header__logo">Where in the world</h1>
+                <h1 className="header__logo">Where in the world?</h1>
                 <button className="header__theme-toggler">
                     <img src={FilledMoonImg} alt="" className="header__theme-toggler__img"/>
                     <span className="header__theme-toggler__text">Dark Mode</span>
