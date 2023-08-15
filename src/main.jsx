1ea50278c622bba30abcae8a19224bf0d13359e9
Suhayb Jirde <dribblekayga@gmail.com>
2023-08-14 21:53:23 -0800

    country-detail error page

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,13 @@ import Home, { loader as homeLoader } from './pages/Home'
 import CountryDetails, {loader as CountryDetailsLoader} from './pages/CoutryDetails'
 import Layout from './components/Layout'
 import NotFound from './pages/NotFound'
+import Error from './pages/Error'
 import './scss/main.scss'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-      <Route index element={<Home />} loader={homeLoader}/>
-      <Route path='/country-details' element={<CountryDetails />} loader={CountryDetailsLoader}/>
+      <Route index element={<Home />} loader={homeLoader} />
+      <Route path='/country-details' element={<CountryDetails />} loader={CountryDetailsLoader} errorElement={<Error />}/>
       <Route path='*' element={<NotFound />} />
     </Route>
   )
---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = ()=> {
+    return (
+        <section className="error">
+            <h1>Sorry! Cant Find This Country</h1>
+        </section>
+    )
+}
+
+export default Error
